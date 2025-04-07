d4845f9e057153ac1f042ee46e98c0a49458333b
LoneDev <leoxy98@gmail.com>
2025-04-07 17:59:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/minecraft-rank-generator',
   plugins: [vue()],
   build: {
     outDir: 'dist'
